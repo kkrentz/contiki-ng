8dfd7849cbf8b9df66e2746fe243790039f62dd5
kkrentz <konrad.krentz@gmail.com>
2025-07-08 20:33:06 +0200

    random: Adopt sfc16 and sfc32 from PractRand

--- os/lib/sfc16-prng.c
+++ os/lib/sfc16-prng.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, Swedish Institute of Computer Science
+ * Copyright (c) 2025, Konrad-Felix Krentz
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,16 +25,58 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
+ */
+
+/**
+ * \addtogroup random
+ * @{
  *
- * This file is part of the Contiki operating system.
- *
+ * \file
+ *         Implements sfc16 of PractRand.
+ * \author
+ *         Konrad Krentz <konrad.krentz@gmail.com>
  */
 
 #include "lib/random.h"
 
+enum {
+  BARREL_SHIFT = 6,
+  RSHIFT = 5,
+  LSHIFT = 3
+};
+
+static uint16_t a;
+static uint16_t b;
+static uint16_t c;
+static uint16_t counter;
+
 /*---------------------------------------------------------------------------*/
-void
-random_init(unsigned short seed)
+static uint_fast16_t
+rand(void)
 {
+  uint16_t tmp = a + b + counter++;
+  a = b ^ (b >> RSHIFT);
+  b = c + (c << LSHIFT);
+  c = ((c << BARREL_SHIFT) | (c >> (16 - BARREL_SHIFT))) + tmp;
+  return tmp;
 }
 /*---------------------------------------------------------------------------*/
+static void
+seed(uint64_t seed)
+{
+  a = seed;
+  b = seed >> 16;
+  c = seed >> 32;
+  counter = seed >> 48;
+  for(uint_fast8_t i = 0; i < 10; i++) {
+    rand();
+  }
+}
+/*---------------------------------------------------------------------------*/
+const struct random_prng sfc16_prng = {
+  seed,
+  rand
+};
+/*---------------------------------------------------------------------------*/
+
+/** @} */
