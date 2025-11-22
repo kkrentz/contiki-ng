4796781f5e35b233308081b4837bea5ae51d68b5
kkrentz <konrad.krentz@gmail.com>
2025-11-22 21:20:14 +0100

    SHA-256: Fix test to resume the hash session before finishing it

--- tests/08-native-runs/14-sha-256/test-sha-256.c
+++ tests/08-native-runs/14-sha-256/test-sha-256.c
@@ -373,14 +373,15 @@ UNIT_TEST(sha_256_hash_with_checkpoint)
     for(size_t j = 0;
         j < sizeof(hashes[i].data) / sizeof(hashes[i].data[0]);
         j++) {
-      SHA_256.restore_checkpoint(&checkpoint);
       if(!hashes[i].data[j]) {
         continue;
       }
+      SHA_256.restore_checkpoint(&checkpoint);
       SHA_256.update((const uint8_t *)hashes[i].data[j],
                      strlen(hashes[i].data[j]));
       SHA_256.create_checkpoint(&checkpoint);
     }
+    SHA_256.restore_checkpoint(&checkpoint);
     uint8_t sha256[SHA_256_DIGEST_LENGTH];
     SHA_256.finalize(sha256);
     UNIT_TEST_ASSERT(!memcmp(sha256, hashes[i].hash, sizeof(sha256)));
